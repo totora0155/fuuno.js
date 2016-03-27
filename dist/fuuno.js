fe16c49ba273b8f406f76f2bd4c66b82519473f4
nju33 <nju33@nmba.local>
2016-03-27 22:07:29 +0900

    Fix

--- dist/fuuno.js
+++ dist/fuuno.js
@@ -18,19 +18,34 @@
   }
 
   function fix(el) {
-    var div = document.createElement('div');
-    el.parentNode.insertBefore(div, el);
-    var parent = document.getElementById('wrapper')
+    var parent = el.parentElement.parentElement;
+    var cushion = document.createElement('div');
+    var offsetHeight = calcOffsetHeight(el, parent.clientHeight);
+    var maxScrollHeight = parent.scrollHeight - offsetHeight;
+
+    el.parentNode.insertBefore(cushion, el);
+
     parent.addEventListener('scroll', function(e) {
-      var offset = el.clientHeight - innerHeight;
-      offset = offset < 0 && 0;
-      if (parent.scrollTop > offset) {
-        var test = -300;
-        div.style.height = parent.scrollTop - offset + test + 'px';
+      if (parent.scrollTop > offsetHeight) {
+        if (parent.scrollTop > maxScrollHeight) {
+          cushion.style.height =  maxScrollHeight + 'px';
+        } else {
+          cushion.style.height = parent.scrollTop - offsetHeight + 'px';
+        }
       } else {
-        div.style.height = 0;
+        cushion.style.height = 0;
       }
     });
+  }
+
+  function calcOffsetHeight(el, parentHeight) {
+    var offsetHeight = null;
+    if (el.scrollHeight > parentHeight) {
+      offsetHeight = el.scrollHeight - parentHeight;
+    } else {
+      offsetHeight = 0;
+    }
+    return offsetHeight;
   }
 
   return foono;
