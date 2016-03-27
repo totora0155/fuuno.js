4446f5070f7face45b86ff4eba3a4981a3679c7c
nju33 <nju33@nmba.local>
2016-03-27 21:32:03 +0900

    Careless

--- dist/fuuno.js
+++ dist/fuuno.js
@@ -11,43 +11,26 @@
 
   function foono(els) {
     var arr = Array.prototype.slice.call(els);
+
     for (var i = 0, len = arr.length; i < len; i++) {
       fix(arr[i]);
     }
   }
 
   function fix(el) {
-    var parent = getScrollElement(el);
-    var height = el.clientHeight;
-
-    parent.addEventListener('scroll', function (e) {
-      var scrollTop = parent.scrollY || parent.scrollTop;
-      var clientHeight = parent.scrollY ? innerHeight : parent.clientHeight;
-      requestAnimationFrame(function() {
-        if (scrollTop > (height - clientHeight)) {
-          el.style.position = 'absolute';
-          el.style.top = scrollTop - (height - clientHeight) + 'px';
-        } else {
-          el.style.top = 0;
-        }
-      });
-    });
-  }
-
-  function getScrollElement(el) {
-    var parent = el.parentElement;
-    while (parent.tagName !== 'BODY') {
-      if (parent.scrollHeight !== parent.clientHeight) {
-        break;
+    var div = document.createElement('div');
+    el.parentNode.insertBefore(div, el);
+    var parent = document.getElementById('wrapper')
+    parent.addEventListener('scroll', function(e) {
+      var offset = el.clientHeight - innerHeight;
+      offset = offset < 0 && 0;
+      if (parent.scrollTop > offset) {
+        var test = -300;
+        div.style.height = parent.scrollTop - offset + test + 'px';
       } else {
-        if (parent.clientHeight === document.body.clientHeight) {
-          parent = window;
-          break;
-        }
+        div.style.height = 0;
       }
-      parent = parent.parentElement;
-    }
-    return parent;
+    });
   }
 
   return foono;
